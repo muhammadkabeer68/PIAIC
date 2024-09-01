c86606ab61103820ef1acd0107b6bbbe3ddd1ff0
muhammadkabeer68 <mlkkabeer68@gmail.com>
2024-08-31 17:12:17 -0700

    python-programming.py

---
+++ python-programming.py
@@ -0,0 +1,141 @@
+print("Hello, World!")
+
+name = "Kabeer"
+print(name)
+
+Firstname = "Malik"
+Lastname = "kabir"
+print(Firstname, Lastname)
+
+# Define a string
+my_string = "Hello, World!"
+
+# Convert to uppercase
+print(my_string.upper())  
+
+# Convert to lowercase
+print(my_string.lower())  
+
+# Convert to title case
+print(my_string.title())  
+
+# Convert to swap case
+print(my_string.swapcase())  
+
+
+name = "Kabir"
+
+print(f"Hello, {name}!") 
+
+#Arithmetic Oprators
+
+# Addition
+x = 5
+y = 3
+print(x + y)  
+
+# Subtraction
+x = 5
+y = 3
+print(x - y)  
+
+# Multiplication
+x = 5
+y = 3
+print(x * y)  
+
+# Division
+x = 10
+y = 2
+print(x / y) 
+
+# Exponentiation
+x = 2
+y = 3
+print(x ** y)  
+
+# Logial operators
+
+# And
+x = 5
+y = 3
+print(x > 3 and y > 2)  
+
+# Or
+x = 5
+y = 3
+print(x > 3 or y > 2)  
+
+# Not
+x = 5
+print(not x > 3)
+
+# Create a list
+fruits = ['apple', 'banana', 'cherry']
+print(fruits)  
+
+# Indexing
+fruits = ['apple', 'banana', 'cherry']
+print(fruits[0]) 
+print(fruits[1])  
+print(fruits[2])  
+
+# Slicing
+fruits = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+print(fruits[1:3])  
+print(fruits[1:])  
+print(fruits[:3])  
+
+# Append
+fruits = ['apple', 'banana', 'cherry']
+fruits.append('date')
+print(fruits)  
+
+# Extend
+fruits = ['apple', 'banana', 'cherry']
+fruits.extend(['date', 'elderberry'])
+print(fruits)  
+
+# Insert
+fruits = ['apple', 'banana', 'cherry']
+fruits.insert(1, 'date')
+print(fruits)  
+
+# Remove
+fruits = ['apple', 'banana', 'cherry']
+fruits.remove('banana')
+print(fruits) 
+
+# Pop
+fruits = ['apple', 'banana', 'cherry']
+print(fruits.pop(1))  
+print(fruits)  
+
+# Index
+fruits = ['apple', 'banana', 'cherry']
+print(fruits.index('banana'))  
+
+# Count
+fruits = ['apple', 'banana', 'banana', 'cherry']
+print(fruits.count('banana'))  
+
+# Sort
+fruits = ['cherry', 'banana', 'apple']
+fruits.sort()
+print(fruits)  
+
+# Reverse
+fruits = ['apple', 'banana', 'cherry']
+fruits.reverse()
+print(fruits) 
+
+
+
+# Basic for loop
+fruits = ['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print(fruit)
+
+
+
+
